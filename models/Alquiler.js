df4d9a1e268d89cf8d9caf3b96fe2218b3b9750e
NazarethV <nazarethvillar38@gmail.com>
2025-01-23 11:04:24 -0300

    model Alquiler (deposit - phoneNumber - checkInTime/checkOutTime - details)

--- models/Alquiler.js
+++ models/Alquiler.js
@@ -2,10 +2,14 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const Alquiler = sequelize.define('Alquiler', {
-  tenantName: { type: DataTypes.STRING, allowNull: false }, // nombre_inquilino -> tenantName
-  startDate: { type: DataTypes.DATE, allowNull: false }, // fecha -> startDate
+  tenantName: { type: DataTypes.STRING, allowNull: false }, // nombre_inquilino
+  startDate: { type: DataTypes.DATE, allowNull: false }, // fecha
   endDate: { type: DataTypes.DATE, allowNull: false }, // fecha fin
-  price: { type: DataTypes.DECIMAL(10, 2), allowNull: false }, // precio -> price
+  price: { type: DataTypes.DECIMAL(10, 2), allowNull: false }, // precio
+  deposit: { type: DataTypes.DECIMAL(10, 2), allowNull: true }, // seña
+  phoneNumber: { type: DataTypes.STRING, allowNull: true }, // número de celular
+  checkInTime: { type: DataTypes.TIME, allowNull: true }, // hora de entrada
+  checkOutTime: { type: DataTypes.TIME, allowNull: true }, // hora de salida
   details: { type: DataTypes.TEXT, allowNull: true }, // detalles
 });
 
