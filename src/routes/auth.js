f41ac09fb4b4e74dbb02965cbeac118af86240cb
NazarethV <nazarethvillar38@gmail.com>
2025-02-10 13:27:46 -0300

    validators

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const { registerHandler, loginHandler } = require('../handlers/authHandler')
+const { registerHandler, loginHandler } = require('../handlers/authHandler');
+const { validateRegister, validateLogin } = require('../validators/authValidator');
 
-router.post('/register', registerHandler);
-router.post('/login', loginHandler);
+router.post('/register', validateRegister, registerHandler);
+router.post('/login', validateLogin, loginHandler);
 
 module.exports = router;
