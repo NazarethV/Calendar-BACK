87055c98dc23137cec0ee90900136b7f99e1caff
NazarethV <nazarethvillar38@gmail.com>
2025-02-10 11:52:19 -0300

    route: auth

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { registerHandler, loginHandler } = require('../handlers/authHandler')
 
 router.post('/register', registerHandler);
 router.post('/login', loginHandler);
